5085f8d5e3350382c5f7521759f17157628bf369
nurungg <wlsdnrqo11@gmail.com>
2018-08-29 12:24:09 +0900

    Add a deleted line

--- algorithm/dftl_fm/dftl.c
+++ algorithm/dftl_fm/dftl.c
@@ -583,6 +583,7 @@ uint32_t __demand_get(request *const req){
     // initialization
     c_table = &CMT[D_IDX];
     p_table = c_table->p_table;
+    t_ppa   = c_table->t_ppa;
 
     // Cache Hit
     if(p_table){
